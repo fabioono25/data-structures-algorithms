d63a2048814710e7ae70da8702719e95c144a8ef
fabioono25 <fabioono25@gmail.com>
2023-12-16 21:16:27 +0100

    single linked list exercise

--- 2-DataStructures/2-Single-Linked-Lists/single-linked-lists.py
+++ 2-DataStructures/2-Single-Linked-Lists/single-linked-lists.py
@@ -2,90 +2,103 @@
 # Each node stores a reference to an object that is an element of the sequence, as well as a reference to the next node of the list.
 
 class Node:
-  def __init__(self, data):
-      self.data = data
-      self.next = None
-  
-  def __repr__(self):
-    return str(self.data)
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+    def __repr__(self):
+        return str(self.data)
+
 
 class SingleLinkedList:
-  def __init__(self):
-    self.head = None
-    self.tail = None
-    self.length = 0
-  
-  # O(1) time complexity
-  def inser_start(self, data):
-    # self.num_nodes += 1
-    new_node = Node(data)
+    def __init__(self):
+        self.head = None
+        self.tail = None
+        self.length = 0
 
-    if self.head is None:
-      self.head = new_node
-      # self.tail = new_node
-    else:
-      new_node.next = self.head
-      self.head = new_node
-    self.length += 1
+    def insert(self, data):
+        self.size += 1
+        new_node = Node(data)
 
-  # O(n) time complexity
-  def insert_end(self, data):
-    new_node = Node(data)
-    if self.head is None:
-      self.head = new_node
-    else:
-      current_node = self.head
+        if not self.head:
+            self.head = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
 
-      # this is why it has O(n) time complexity
-      while current_node.next is not None:
-        current_node = current_node.next
+    # O(1) time complexity
 
-      current_node.next = new_node
-    self.length += 1
+    def inser_start(self, data):
+        # self.num_nodes += 1
+        new_node = Node(data)
 
-  # O(n) time complexity
-  def insert_at(self, data, position):
-    new_node = Node(data)
-    if position == 0:
-      self.inser_start(data)
-    elif position == self.length:
-      self.insert_end(data)
-    else:
-      current_node = self.head
-      current_position = 0
-      while current_position < position - 1:
-        current_node = current_node.next
-        current_position += 1
-      
-      new_node.next = current_node.next
-      current_node.next = new_node
-      self.length += 1
+        if self.head is None:
+            self.head = new_node
+            # self.tail = new_node
+        else:
+            new_node.next = self.head
+            self.head = new_node
+        self.length += 1
 
-  def remove(self, data):
-    if self.head is None:
-      return None
-    
-    actual_node = self.head
-    previous_node = None
+    # O(n) time complexity
+    def insert_end(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            current_node = self.head
 
-    while actual_node is not None and actual_node.data != data:
-      previous_node = actual_node
-      actual_node = actual_node.next
+            # this is why it has O(n) time complexity
+            while current_node.next is not None:
+                current_node = current_node.next
 
-    if actual_node is None:
-      return
-    
-    if previous_node is None:
-      self.head = actual_node.next
-    else:
-      previous_node.next = actual_node.next # GC will remove the node
+            current_node.next = new_node
+        self.length += 1
 
-  # O(n) time complexity
-  def traverse(self):
-    current_node = self.head
-    while current_node is not None:
-      print(current_node.data)
-      current_node = current_node.next
+    # O(n) time complexity
+    def insert_at(self, data, position):
+        new_node = Node(data)
+        if position == 0:
+            self.inser_start(data)
+        elif position == self.length:
+            self.insert_end(data)
+        else:
+            current_node = self.head
+            current_position = 0
+            while current_position < position - 1:
+                current_node = current_node.next
+                current_position += 1
+
+            new_node.next = current_node.next
+            current_node.next = new_node
+            self.length += 1
+
+    def remove(self, data):
+        if self.head is None:
+            return None
+
+        actual_node = self.head
+        previous_node = None
+
+        while actual_node is not None and actual_node.data != data:
+            previous_node = actual_node
+            actual_node = actual_node.next
+
+        if actual_node is None:
+            return
+
+        if previous_node is None:
+            self.head = actual_node.next
+        else:
+            previous_node.next = actual_node.next  # GC will remove the node
+
+    # O(n) time complexity
+    def traverse(self):
+        current_node = self.head
+        while current_node is not None:
+            print(current_node.data)
+            current_node = current_node.next
+
 
 # Test
 my_list = SingleLinkedList()
@@ -100,4 +113,3 @@ my_list.remove(100)
 my_list.remove("John")
 my_list.remove(True)
 my_list.traverse()
-
