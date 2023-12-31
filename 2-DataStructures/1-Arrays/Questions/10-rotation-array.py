50259ddfccce4e2cc55434e006b20e6608b7a3db
fabioono25 <fabioono25@gmail.com>
2023-12-31 10:49:48 +0100

    rotate items

--- 2-DataStructures/1-Arrays/Questions/10-rotation-array.py
+++ 2-DataStructures/1-Arrays/Questions/10-rotation-array.py
@@ -2,11 +2,16 @@
 # Example 1: [1,2,3,4,5] -> [5,1,2,3,4]
 # Example 2: [9,8,7,6,4,2,1,3] -> [3,9,8,7,6,4,2,1]
 
+# Optimized solution
 # Time complexity: O(n)
 # Space complexity: O(1)
 def rotate(arr, n):
-    # solve it
-    return None
+    last = arr[n-1]
+    for i in range(n-1, 0, -1):
+        arr[i] = arr[i-1]
+    arr[0] = last
+
+    return arr
 
 if __name__ == "__main__":
   # Driver function
