af24092e69622778c40963b9eb845fc70bf3c9da
fabioono25 <fabioono25@gmail.com>
2024-01-02 21:07:03 +0100

    exercises

---
+++ 2-DataStructures/2-Linked-Lists/3-Circular-LinkedLists/Questions/1-check-circular-ll.py
@@ -0,0 +1,29 @@
+# Given a singly linked list, find if the list is a circular linked list or not.
+# A circular linked list is one where the last node of the list points back to the first node of the list.
+# Example: A -> B -> C -> A -> B -> C -> A -> ...
+# An empty list is considered a circular linked list.
+
+
+
+if __name__ == '__main__':
+    # llist = LinkedList()
+    # llist.head = Node(1)
+    # second = Node(2)
+    # third = Node(3)
+    # fourth = Node(4)
+
+    # llist.head.next = second
+    # second.next = third
+    # third.next = fourth
+
+    # if (Circular(llist.head)):
+    #     print('Yes')
+    # else:
+    #     print('No')
+
+    # fourth.next = llist.head
+
+    # if (Circular(llist.head)):
+    #     print('Yes')
+    # else:
+    #     print('No')
---
+++ 2-DataStructures/2-Linked-Lists/3-Circular-LinkedLists/Questions/2-count-nodes-circular-ll.py
@@ -0,0 +1,14 @@
+# given a circular linked list, count the number of nodes in it.
+# example: A -> B -> C -> A -> B -> C -> A -> ... the number of nodes is 3
+
+
+# if __name__=='__main__': 
+ 
+#     # Initialize lists as empty */
+#     head = None
+#     head = push(head, 12)
+#     head = push(head, 56)
+#     head = push(head, 2)
+#     head = push(head, 11)
+ 
+#     print( countNodes(head))
